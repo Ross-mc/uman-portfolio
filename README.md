9ec38360746a3837f8eeea15bcdcd692b7747027
Ross McIntyre <rossmci90@hotmail.co.uk>
2020-10-28 14:15:07 +0000

    First Commit with first page completed

---
+++ README.md
@@ -0,0 +1,5 @@
+# Portfolio Page
+
+This repository contains the code for my portfolio page displaying the various projects completed as part of the University of Manchester Coding Bootcamp.
+
+The live page is available (tbc)
---
+++ assets/script.js
@@ -0,0 +1,47 @@
+$(function(){
+    $(".navLink").mouseenter(function(){
+        $(".current").css("color", "whitesmoke");
+    });
+    $(".navLink").mouseleave(function(){
+        $(".current").css("color", "#008B8B");
+    });
+
+    $("#dropbtn").click(function(){
+        $(".dropdown-link").css("display", "block").animate({height: "33vh"}, 1000);
+        $("#dropbtn").css("display", "none");
+        $("#close").css("display", "block")
+    });
+
+    $("#close, .current").click(function(){
+        $(".dropdown-link").slideUp(1000).animate({height: "1vh"}, 1)
+        setTimeout(function(){$("#dropbtn").css("display", "block")}, 1000);
+        $("#close").css("display", "none");
+    });
+
+    $(".portfolio-img").mouseenter(function(){
+        $(this).css("box-shadow", "0 0 3pt 2pt #008B8B");
+    });
+
+    $(".portfolio-img").mouseleave(function(){
+        $(this).css("box-shadow", "");
+    });
+
+    $(".code-btn").mouseenter(function(){
+        $(this).height(function(index, height){
+            return (height + 10);
+        });
+        $(this).width(function(index, width){
+            return (width + 10);
+        });
+    });
+
+    $(".code-btn").mouseleave(function(){
+        $(this).height(function(index, height){
+            return (height - 10);
+        });
+        $(this).width(function(index, width){
+            return (width - 10);
+        });
+    });
+    
+});
